7685c511f5fc36b9399d7e6c85a96b54dce0cc4b
Disna kokkat Devassykutty <81401925+Disna11@users.noreply.github.com>
2023-11-23 13:13:23 +0000

    just to learn

---
+++ app/src/main/java/com/example/review/MainActivity12.kt
@@ -0,0 +1,11 @@
+package com.example.review
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity12 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main12)
+    }
+}
