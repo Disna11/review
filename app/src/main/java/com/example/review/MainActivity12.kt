73d9a69c5276a2430c82cf67f70b63188c19d899
Disna kokkat Devassykutty <81401925+Disna11@users.noreply.github.com>
2023-11-23 13:14:01 +0000

    just to learn2

--- app/src/main/java/com/example/review/MainActivity12.kt
+++ app/src/main/java/com/example/review/MainActivity12.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity12 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main12)
+        setContentView(R.layout.activity_main12);
+        ggggg
     }
 }
